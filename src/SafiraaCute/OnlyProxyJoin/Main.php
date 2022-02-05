edada10e7cf9a1256d7148798fbda770b25ea8f1
SafiraaCute <71029264+SafiraaCute@users.noreply.github.com>
2022-02-05 23:25:19 +0800

    Add files via upload

--- src/SafiraaCute/OnlyProxyJoin/Main.php
+++ src/SafiraaCute/OnlyProxyJoin/Main.php
@@ -1,1 +1,26 @@
+<?php
 
+namespace LittleAraaCute\OnlyProxyJoin;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+use pocketmine\event\player\PlayerPreLoginEvent;
+
+class Main extends PluginBase implements Listener {
+	
+	public function onEnable(): void {
+		$this->getLogger()->info("Plugin Enabled by LittleAraaCute");
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveResource("config.yml");
+        $this->getConfig = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+	}
+	
+	public function onPreLogin(PlayerPreLoginEvent $event){
+		if($event->getIp() == $this->getConfig()->get("proxy-ip")){
+			$event->setKickReason(0, $this->getConfig()->get("kick-message"));
+		}
+	}
+}
